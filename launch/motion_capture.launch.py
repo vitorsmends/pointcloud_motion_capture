65a1260f39d47e1a5433014725dedb5e67c285e5
mendes <vitor.mendes@ieee.org>
2024-03-17 18:51:06 -0300

    fix launch file

--- launch/motion_capture.launch.py
+++ launch/motion_capture.launch.py
@@ -1,24 +1,21 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    pkg_share_dir = os.path.join(
+        get_package_share_directory('pointcloud_motion_capture'))
+
     return LaunchDescription([
         Node(
-            package='realsense2_camera',
-            executable='rs_pointcloud_launch.py',
-            name='realsense2_camera',
-            output='screen'
+            package='pointcloud_motion_capture',
+            executable='point_cloud_reader', 
+            output='screen',
         ),
+
         Node(
             package='pointcloud_motion_capture',
-            executable='point_cloud_reader',
-            name='point_cloud_reader',
-            output='screen'
+            executable='object_detection', 
+            output='screen',
         ),
-        Node(
-            package='pointcloud_motion_capture',
-            executable='motion_capture',
-            name='motion_capture',
-            output='screen'
-        )
     ])
