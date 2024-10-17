7f198e4672c763fcc8378623755b8fc8918f5e1a
mendes <vitor.mendes@ieee.org>
2024-10-17 10:20:19 -0300

    add ros parameters

--- include/point_cloud_reader.hpp
+++ include/point_cloud_reader.hpp
@@ -15,20 +15,36 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 
+namespace pointcloud_motion_capture
+{
 class PointCloudProcessor : public rclcpp::Node {
-public:
+ public:
     PointCloudProcessor();
 
-private:
+ private:
+    const int kRedThresholdDefault {40};
+    const int kGreenThresholdDefault {30};
+    const int kBlueThresholdDefault {30};
+    const float kUpdateRate {10.0};
+    const std::string kPointCloudTopicRaw {"/camera/camera/depth/color/points"};
+    const std::string kPointCloudTopicFiltered {"/point_cloud/filtered"};
+
     void load_parameters();
     void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
 
-    int red_threshold_;
-    int green_threshold_;
-    int blue_threshold_;
+    int red_threshold_ {};
+    int green_threshold_ {};
+    int blue_threshold_ {};
+    std::string frame_id_ {"camera_link"};
+    std::string point_cloud_topic_raw_ {};
+    std::string point_cloud_topic_filtered_ {};
+    float update_rate_ {};
+    sensor_msgs::msg::PointCloud2 filtered_msg_;
 };
 
-#endif // POINT_CLOUD_READER_HPP
+}  // namespace pointcloud_motion_capture
+
+#endif  // POINT_CLOUD_READER_HPP
--- src/point_cloud_reader.cpp
+++ src/point_cloud_reader.cpp
@@ -1,30 +1,45 @@
 #include "point_cloud_reader.hpp"
 
+namespace pointcloud_motion_capture
+{
 PointCloudProcessor::PointCloudProcessor() : Node("point_cloud_processor") {
     load_parameters();
 
     subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        "/camera/camera/depth/color/points", 10,
+        point_cloud_topic_raw_, update_rate_,
         std::bind(&PointCloudProcessor::pointCloudCallback, this, std::placeholders::_1));
 
     publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-        "/point_cloud/filtered", 10);
+        point_cloud_topic_filtered_, update_rate_);
 
     RCLCPP_INFO(this->get_logger(), "PointCloudProcessor node initialized.");
 }
 
 void PointCloudProcessor::load_parameters() {
-    std::string yaml_file_path =
-        ament_index_cpp::get_package_share_directory("pointcloud_motion_capture") +
-        "/config/colors.yaml";
-    std::cout << yaml_file_path << std::endl;
+    this->declare_parameter<int>("red_threshold", kRedThresholdDefault);
+    this->declare_parameter<int>("green_threshold", kGreenThresholdDefault);
+    this->declare_parameter<int>("blue_threshold", kBlueThresholdDefault);
+    this->declare_parameter<std::string>("point_cloud_topic_raw", kPointCloudTopicRaw);
+    this->declare_parameter<std::string>("point_cloud_topic_filtered", kPointCloudTopicFiltered);
+    this->declare_parameter<float>("update_rate", kUpdateRate);
 
-    YAML::Node config = YAML::LoadFile(yaml_file_path);
+    this->get_parameter("red_threshold", red_threshold_);
+    this->get_parameter("green_threshold", green_threshold_);
+    this->get_parameter("blue_threshold", blue_threshold_);
+    this->get_parameter("point_cloud_topic_raw", point_cloud_topic_raw_);
+    this->get_parameter("point_cloud_topic_filtered", point_cloud_topic_filtered_);
+    this->get_parameter("update_rate", update_rate_);
 
-    red_threshold_ = config["red_threshold"].as<int>();
-    green_threshold_ = config["green_threshold"].as<int>();
-    blue_threshold_ = config["blue_threshold"].as<int>();
+    RCLCPP_INFO(this->get_logger(), "Loading Parameters...");
+    RCLCPP_INFO(this->get_logger(), "Filter parameters:");
+    RCLCPP_INFO(this->get_logger(), "   Red threshold: %d", red_threshold_);
+    RCLCPP_INFO(this->get_logger(), "   Green threshold: %d", green_threshold_);
+    RCLCPP_INFO(this->get_logger(), "   Blue threshold: %d", blue_threshold_);
+    RCLCPP_INFO(this->get_logger(), "Reading point cloud from: %s", point_cloud_topic_raw_.c_str());
+    RCLCPP_INFO(this->get_logger(), "Publishing filtered point cloud on: %s", point_cloud_topic_filtered_.c_str());
+    RCLCPP_INFO(this->get_logger(), "Update Rate: %f Hz", update_rate_);
 }
+
 
 void PointCloudProcessor::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
@@ -37,17 +52,18 @@ void PointCloudProcessor::pointCloudCallback(const sensor_msgs::msg::PointCloud2
         }
     }
 
-    sensor_msgs::msg::PointCloud2 filtered_msg;
-    pcl::toROSMsg(*filtered_cloud, filtered_msg);
-    filtered_msg.header.frame_id = "camera_link";
-    filtered_msg.header.stamp = this->get_clock()->now();
+    pcl::toROSMsg(*filtered_cloud, filtered_msg_);
+    filtered_msg_.header.frame_id = frame_id_;
+    filtered_msg_.header.stamp = this->get_clock()->now();
 
-    publisher_->publish(filtered_msg);
+    publisher_->publish(filtered_msg_);
 }
+
+} // namespace pointcloud_motion_capture
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PointCloudProcessor>());
+    rclcpp::spin(std::make_shared<pointcloud_motion_capture::PointCloudProcessor>());
     rclcpp::shutdown();
     return 0;
 }
