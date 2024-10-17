ad025b095c7a39b8198d44b0836055520aa3dee3
mendes <vitor.mendes@ieee.org>
2024-10-17 17:20:30 -0300

    apply linter output in cmake and launch file

--- launch/motion_capture.launch.py
+++ launch/motion_capture.launch.py
@@ -1,7 +1,9 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     pkg_share_dir = os.path.join(
