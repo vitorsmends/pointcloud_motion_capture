aa354ed7c3c3b98fb79d1294cd603752042630b2
mendes <vitor.mendes@ieee.org>
2024-10-17 11:06:24 -0300

    fix ros parameters import

--- launch/motion_capture.launch.py
+++ launch/motion_capture.launch.py
@@ -7,16 +7,19 @@ def generate_launch_description():
     pkg_share_dir = os.path.join(
         get_package_share_directory('pointcloud_motion_capture'))
 
+    config_file = os.path.join(pkg_share_dir, 'config', 'config.yaml')
+
     return LaunchDescription([
         Node(
             package='pointcloud_motion_capture',
-            executable='point_cloud_reader', 
+            executable='point_cloud_reader',
             output='screen',
+            parameters=[config_file],
         ),
 
         Node(
             package='pointcloud_motion_capture',
-            executable='motion_capture', 
+            executable='motion_capture',
             output='screen',
         ),
     ])
